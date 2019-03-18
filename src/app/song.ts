b8e6f35f68fa14333cc9ca4741b5a6b754b4604c
hophiducanh <contact.hoducanh@gmail.com>
2019-03-18 11:27:25 +0700

    add song.ts

--- src/app/song.ts
+++ src/app/song.ts
@@ -1,12 +1,13 @@
 import {IImage} from './image';
 import {ISongCategory} from './song-category';
+import {IMp3File} from './mp3-file';
 
 export interface ISong {
   id: number;
   name: string;
   description: string;
   singer_name: string;
-  mp3_link: string;
+  mp3File: IMp3File;
   image: IImage;
   songCategory: ISongCategory;
 }
