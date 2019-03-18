48b2161ed26acbc3265f7152ffa309b37531bd0a
datquocngo <shaco14091999@gmail.com>
2019-03-18 13:21:15 +0700

    add create component demo

--- src/app/admin/create-song/create-song.component.ts
+++ src/app/admin/create-song/create-song.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ISong} from '../../song';
+import {SongService} from '../../song.service';
+import {ImageService} from '../../image.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-create-song',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateSongComponent implements OnInit {
 
-  constructor() { }
+  formCreate: FormGroup;
+  fileImg: File;
+  fileMp3: File;
+  song: ISong;
+
+  constructor(
+    private songService: SongService,
+    private imageService: ImageService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+  }
 
   ngOnInit() {
+    this.formCreate = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      description: ['', [Validators.required, Validators.minLength(3)]],
+      singer_name: ['', [Validators.required, Validators.minLength(3)]],
+      nameImg: [''],
+    });
   }
 
 }
